e5a839c22af9bbb4ce640b7101372799f9ed4c54
thalgi <thalgi.muller@gmail.com>
2025-06-30 14:47:50 +0200

    GLTF SECTION OK

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import MainSection from './components/main_section.jsx';
 import TeamSection from './components/team_section.jsx';
 import ProjectsSection from './components/projects_section.jsx';
 import Cliff_Back from './components/cliff_parallax_base';
-import GLTFViewer from "./components/GLTFViewer_simple"
 import GLTFSection from './components/GLTFViewer_section';
 import Horizontal_Scroller from "./components/text_scroller"
 import Footer from "./components/footer"
@@ -22,21 +21,31 @@ class App extends React.Component {
 
                 <TeamSection />
 
-                <GLTFSection config={{
+                <GLTFSection 
+                sectionId="middle-section"
+                config={{
                     modelPath: '/scene/evershapes_scene2.gltf',
                     cameraAngle: 30,
-                    cameraDistance: 4,
+                    cameraDistance: 4.5,
                     autoRotateSpeed: 0.005,
                     modelRotation: 90,
                     modelScale: 5
 
 
                 }}
-                background='radial-gradient(ellipse 60% 30% at center, #00AFB9 0%, #00AFB9 55%, #FDFCDC 60%)'
+                ellipseGradient='repeating-linear-gradient(174deg, #0322F912 92%, #073AFF00 100%),
+                repeating-linear-gradient(49deg, #F9E50373 99%, #073AFF00 100%),
+                repeating-radial-gradient(75% 75% at 238% 218%, #F6222212 37%, #073AFF14 39%),
+                radial-gradient(99% 99% at 109% 2%, #FCFF00FF 0%, #FFFC0000 100%),
+                radial-gradient(99% 99% at 21% 78%, #A76AEFA6 0%, #073AFF00 100%),
+                radial-gradient(160% 154% at 711px -303px, #FC7B00FF 0%, #FFA200FF 100%)'
+                overlayText="Live Trough a Journey"
                 />
                 <ProjectsSection />
 
-                <GLTFSection config={{
+                <GLTFSection 
+                sectionId="bottom-section"
+                config={{
                     modelPath: '/scene/evershapes_scene3.gltf',
                     cameraAngle: 30,
                     cameraDistance: 4,
@@ -44,7 +53,12 @@ class App extends React.Component {
                     modelRotation: 90,
                     modelScale: 5
                 }}
-                    background='radial-gradient(ellipse 60% 30% at center, #00AFB9 0%, #00AFB9 55%, #FDFCDC 60%)' />
+                ellipseGradient='repeating-radial-gradient(75% 75% at 238% 218%, #00FFFF12 30%, #073AFF14 39%),
+                radial-gradient(99% 99% at 109% 2%, #0F00A9FF 0%, #073AFF00 100%),
+                radial-gradient(99% 99% at 21% 78%, #7D00FFFF 0%, #073AFF00 100%),
+                radial-gradient(160% 154% at 711px -303px, #2000FFFF 0%, #000000FF 100%)' 
+                    overlayText="Fights Epic Battles"
+                    />
                 <Footer />
 
                 <Horizontal_Scroller />
